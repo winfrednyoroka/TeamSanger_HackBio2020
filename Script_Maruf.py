8330d713ff5c21cb0562035becd08833564687d9
Maruf Ahmed Bhuiyan <64158584+maruf-ahmed-bhuiyan@users.noreply.github.com>
2020-08-04 14:36:29 +0600

    Add files via upload

---
+++ Script_Maruf.py
@@ -0,0 +1,13 @@
+name = 'Maruf Ahmed Bhuiyan'
+
+email = 'dr.marufahmed89@gmail.clm'
+
+language = 'Python'
+
+biostack = 'Genomics'
+
+slack = '@Maruf'
+
+
+
+print("Name: {}\nEmail: {}\nLanguage: {}\nBiostack: {}\nSlack Username: {}".format(name, email, language, biostack, slack))
