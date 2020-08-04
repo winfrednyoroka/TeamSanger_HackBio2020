15c0ab5401f7c673e44838a2886195b32ba6c11f
Maruf Ahmed Bhuiyan <64158584+maruf-ahmed-bhuiyan@users.noreply.github.com>
2020-08-04 22:18:13 +0600

    Delete script_Maruf.pl

--- script_Maruf.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/perl
-
-$name = 'Maruf Ahmed Bhuiyan';
-$email = 'dr.marufahmed89@gmail.com';
-$language = 'Perl';
-$biostack = 'Genomics';
-$slack = '@Maruf';
-
-print "Name: $name, Email: $email, Language: $language, Biostack: $biostack, Slack_Uername: $slack";
