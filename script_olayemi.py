5c64d2df0538b5562fd9a2f5bd9f6e038c33acf0
ola-dav <69109735+ola-dav@users.noreply.github.com>
2020-08-04 14:42:44 +0100

    Update script_olayemi.py

--- script_olayemi.py
+++ script_olayemi.py
@@ -1,14 +1,10 @@
 #!bin/python
 #this is a python file of hello world
 
-def personal_details():
-    Name = "Olayemi_ROTIMI"
-    Email = "olayemirotimi3@gmail.com"
-    Language = "python"
-    Biostack = "Genomics"
-    Slack_username = "@Olayemi"
-    
-    print("Name: " + Name + "\nEmail: " + Email + "\nLanguage: " + Language
-          + "\nBiostack: " + Biostack + "\nSlack_username: " + Slack_username)
-          
-personal_details()
+name= "olayemi_rotimi"
+email="olayemirotimi3@gmail.com"
+language="python"
+biostack="genomics"
+slack="olayemi"
+print("name: {}\nemail: {}\nlanguage: {}\nbiostack: {}\nslackname: {}" . 
+      format(name , email , language , biostack ,slack))
