64cac252716a25977ec98983ef5e3cdf7146cbf4
AyodeleO20 <69175994+AyodeleO20@users.noreply.github.com>
2020-08-04 12:02:16 +0100

    Add files via upload

---
+++ Script_Ayodele.R
@@ -0,0 +1,7 @@
+name <- "Oguntuase Ayodele"
+email <- "ayodeleemmanuel806gmail.com"
+biostack <- "Genomics"
+username <- "@Ayodele"
+language <- "R"
+
+print(paste("name:", name, "email:", email, "biostack:", biostack, "username:", username, "language:", language, Sep="ln"))
