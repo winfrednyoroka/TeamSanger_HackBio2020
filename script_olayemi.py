6bd45d2786a5a8d75bf893a848d3aeda33729eca
ola-dav <69109735+ola-dav@users.noreply.github.com>
2020-08-04 13:08:02 +0100

    Add files via upload

---
+++ script_olayemi.py
@@ -0,0 +1,14 @@
+#!bin/python
+#this is a python file of hello world
+
+def personal_details():
+    Name = "Olayemi_ROTIMI"
+    Email = "olayemirotimi3@gmail.com"
+    Language = "python"
+    Biostack = "Genomics"
+    Slack_username = "@Olayemi"
+    
+    print("Name: " + Name + "\nEmail: " + Email + "\nLanguage: " + Language
+          + "\nBiostack: " + Biostack + "\nSlack_username: " + Slack_username)
+          
+personal_details()
