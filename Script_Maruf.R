41862eb73f00118238313137bef77f467e2ea762
Maruf Ahmed Bhuiyan <64158584+maruf-ahmed-bhuiyan@users.noreply.github.com>
2020-08-04 20:02:02 +0600

    Delete Script_Maruf.R

--- Script_Maruf.R
+++
@@ -1,9 +0,0 @@
-name <- "Maruf Ahmed Bhuiyan"
-email <- "dr.marufahmed89@gmail.com"
-biostack <- "Genomics"
-username <- "@Maruf"
-language <- "R"
-
-cat(paste("Name:", name,"Email:", email, 
-          "Biostack:", biostack, "Username:", username,
-          "Language:", language, sep="\n"))
