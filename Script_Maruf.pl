4d9dbe89439e72ea742ee20baa2bd0468ca0f2a2
Maruf Ahmed Bhuiyan <64158584+maruf-ahmed-bhuiyan@users.noreply.github.com>
2020-08-04 20:44:41 +0600

    Add files via upload

---
+++ Script_Maruf.pl
@@ -0,0 +1,13 @@
+#!/usr/local/bin/perl
+
+$name = 'Maruf Ahmed Bhuiyan';
+
+$email = 'dr.marufahmed89@gmail.com';
+
+$language = 'Perl';
+
+$biostack = 'Genomics';
+
+$slack = '@Maruf';
+
+print "Name: $name.\nEmail: $email.\nLanguage: $language.\nBiostack: $biostack\nSlack_Uername: $slack";
---
+++ Script_Maruf.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3
+#this is a python file of hello world
+
+name = "Maruf Ahmed Bhuiyan"
+email = "dr.marufahmed89@gmail.com"
+language = "Python"
+biostack = "Genomics"
+slack = "@Maruf"
+
+print("Name: {}\nEmail: {}\nLanguage: {}\nBiostack: {}\nSlack Username: {}".format(name, email, language, biostack, slack))
