3ba37454be83456e1caf70c92fad5be4db913557
Maruf Ahmed Bhuiyan <64158584+maruf-ahmed-bhuiyan@users.noreply.github.com>
2020-08-04 14:20:34 +0600

    Add files via upload

---
+++ Script_Maruf.R
@@ -0,0 +1,9 @@
+name <- "Maruf Ahmed Bhuiyan"
+email <- "dr.marufahmed89@gmail.com"
+biostack <- "Genomics"
+username <- "@Maruf"
+language <- "R"
+
+cat(paste("Name:", name,"Email:", email, 
+          "Biostack:", biostack, "Username:", username,
+          "Language:", language, sep="\n"))
