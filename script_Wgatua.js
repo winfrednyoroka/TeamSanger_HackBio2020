1767a2db3492837bcfd4b30d5a069762e3f9326a
Winfred Gatua <40457085+winfrednyoroka@users.noreply.github.com>
2020-08-05 07:21:50 +0300

    Add files via upload
    adding my javascript into the Repo

---
+++ script_Wgatua.js
@@ -0,0 +1,14 @@
+#!/usr/local/bin/node
+// this script prints my personal detail
+
+// assigning some variables of my personal information
+var name = "Winfred Gatua";
+var email = "Winfredgatua13@gmail.com";
+var lang = "javascript";
+var biostack = "Genomics";
+var disp_name = "@WGatua";
+
+console.log (name +', ' + email + ', '+ lang +', '+ biostack +', '+ disp_name);
+
+
+
