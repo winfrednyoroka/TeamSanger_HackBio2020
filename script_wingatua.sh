55d61cfaa540a8db0bcb858bbe08d00d52ba7180
Winfred Gatua <40457085+winfrednyoroka@users.noreply.github.com>
2020-08-05 22:01:41 +0300

    Add files via upload
    a bash file printing same information

---
+++ script_wingatua.sh
@@ -0,0 +1,6 @@
+#that prints your name, your email, the language you used, your biostack and your slack username (with @)
+
+#!/usr/bin/env bash
+
+echo "Winfred Gatua, winfredgatua13@gmail.com, bash, genomics, @WGatua"
+ 
