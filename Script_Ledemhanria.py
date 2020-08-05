548f8f0dea1fecc5496fc2c6a797a5b502239d68
Ledemhanria <69086684+Ledemhanria@users.noreply.github.com>
2020-08-05 15:55:25 +0100

    Add files via upload

---
+++ Script_Ledemhanria.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Prints out my name, email, language, Biostack and Slackusername
+
+def personal_details ():
+	Name = "Edemhanria_Lawrence"
+	Email = "ledemhanria@gmail.com"
+	Language = "python"
+	Biostack = "Genomics"
+	Slack_username = "@Ledemhanria"
+	print ("Name: {}\nEmail: {}\nLanguage: {}\nBiostack: {}\nSlack_username: {}".format(Name,Email,Language,Biostack,Slack_username))
+personal_details ()
