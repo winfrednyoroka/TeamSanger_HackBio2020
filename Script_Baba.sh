8ed619cfac0b9be3d1133356108ab6a29e2907bc
Umar Ahmad <45367984+babasaraki@users.noreply.github.com>
2020-08-06 02:35:10 +0800

    Delete Script_Baba.sh

--- Script_Baba.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#A written script for printing my personal profile at HackBio Internship 2020
-
-#Print my profile
- echo "Umar Ahmad, babasaraki@yahoo.co.uk, Bash, Genomics, @Baba"
-
-#End scripting
