590974180c3484d6f67d00a33790f3534542f831
Umar Ahmad <45367984+babasaraki@users.noreply.github.com>
2020-08-06 02:33:41 +0800

    Script_Baba.sh

--- Script_Baba.sh
+++ Script_Baba.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#A written script for priting my personal profile for HackBio Internship 2020
+#A written script for printing my personal profile at HackBio Internship 2020
 
 #Print my profile
  echo "Umar Ahmad, babasaraki@yahoo.co.uk, Bash, Genomics, @Baba"
