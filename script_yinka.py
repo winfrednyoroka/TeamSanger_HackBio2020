0f65edaf2680b2a5049705a4ad09bccdc1bebda0
Yinkah15 <69058397+Yinkah15@users.noreply.github.com>
2020-08-04 22:28:45 -0700

    Update script_yinka.py

--- script_yinka.py
+++ script_yinka.py
@@ -1,4 +1,4 @@
 #!/bin/python
 #Script printing my personal details
 ​
-print("Ibiyinka Daramola,yinkledeste@gmail.com,Bash,Genomics,@Yinka")
+print("Ibiyinka Daramola,yinkledeste@gmail.com,Python,Genomics,@Yinka")
