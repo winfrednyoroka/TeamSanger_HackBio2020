cc125da349a37c3396c5b597b33ed0e0c9c99d61
Ankita Kumari <letsomics@gmail.com>
2020-08-05 22:29:00 +0530

    Add files via upload

---
+++ script_ankita.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#My First Script for TeamSanger_HackBio2020
+#task 1 for week 0
+
+#print details
+echo "Ankita Kumari, letsomics@gmail.com, bash, genomics, @ankita"
+
+#end of script
