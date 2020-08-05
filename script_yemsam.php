8a0cc7eb01d7470144aaa5e019236dc6a786245a
Olayemi Olaoluwa Samuel <66535994+Yemsam@users.noreply.github.com>
2020-08-05 19:34:40 +0100

    Add files via upload
    Adding script_yemsam

---
+++ script_yemsam.php
@@ -0,0 +1,9 @@
+<?php
+$name = 'Olayemi Olaoluwa Samuel';
+$email = 'olayemioluwa@gmail.com';
+$language_used = 'PHP';
+$biostack = 'Data Analytics';
+$slack_username = 'Yemsam';
+
+printf ("%s, %s, %s, %s, @%s", $name, $email, $language_used, $biostack, $slack_username);
+?>
