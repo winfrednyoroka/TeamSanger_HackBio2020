68bdccd35bb040de12edf2ff0066e36068f5ab3c
chavhanrj <69244764+chavhanrj@users.noreply.github.com>
2020-08-05 21:50:19 +0530

    Add files via upload

---
+++ Script_Rajesh.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#my personal details
+name="Rajesh_chavhan"
+email="rajshrichavhan143@gmail.com"
+language="python3"
+biostack="genomics"
+slack_username="Rajesh"
+print(f"Name:{name}\n Email:{email}\n Language:{language}\n Biostack:{biostack}\n Slack_username:{slack_username}")
