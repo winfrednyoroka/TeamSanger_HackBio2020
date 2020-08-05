d0b73a7d700fc78cbe46a1f9837e0d854131fd03
Raplic <69117766+Raplic@users.noreply.github.com>
2020-08-05 22:04:29 +0100

    Update script_Semilogo.c
    Modified printf statement for output to align with team defined format

--- script_Semilogo.c
+++ script_Semilogo.c
@@ -4,8 +4,8 @@ int main (){
 	char *name = "Semilogo Sonaike";
 	char *email = "semilogosonaike@gmail.com";
 	char *language = "C";
-	char *biostack = "Science Communication";
+	char *biostack = "Genomics";
 	char *slackUsername = "@Semilogo";
-	printf("\%s\n\%s\n\%s\n\%s\n\%s",name,email,language,biostack,slackUsername);
+	printf("\%s,\%s,\%s,\%s,\%s\n",name,email,language,biostack,slackUsername);
 	return 0;
 }
