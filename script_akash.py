08425382befa1fc399e72bdedcee05dac0e95e8c
Winfred Gatua <40457085+winfrednyoroka@users.noreply.github.com>
2020-08-06 18:21:20 +0300

    Add files via upload

---
+++ script_akash.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#my personal details
+name="akash_chavhan"
+email="chavhanab999@gmail.com"
+language="python3"
+biostack="genomics"
+slack_username="@Akash"
+print ("{}, {}, {}, {}, {}".format(name, email, language, biostack, slack_username))
