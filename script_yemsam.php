37ab8eb10dfdb437bd03d97161c2f23f3c47768f
Winfred Gatua <40457085+winfrednyoroka@users.noreply.github.com>
2020-08-06 16:57:52 +0300

    uploading this script

---
+++ script_yemsam.php
@@ -0,0 +1,11 @@
+#!/usr/bin/php
+<?php
+
+$name = 'Olayemi Olaoluwa Samuel';
+$email = 'olayemioluwa@gmail.com';
+$language_used = 'PHP';
+$biostack = 'Data Analytics';
+$slack_username = 'Yemsam';
+
+echo "$name, $email, $language_used, $biostack, $slack_username";
+?>
