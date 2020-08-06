4006df8bfc0ebaa2fa8ad4a14d95682bd7c71e1d
Winfred Gatua <40457085+winfrednyoroka@users.noreply.github.com>
2020-08-06 17:14:59 +0300

    Update script_Rajesh.py

--- script_Rajesh.py
+++ script_Rajesh.py
@@ -5,4 +5,4 @@ email="rajshrichavhan143@gmail.com"
 language="python3"
 biostack="genomics"
 slack_username="@Rajesh"
-print(f"{name}{email}{language}{biostack}{slack_username}")
+print(f"{name}, {email}, {language}, {biostack}, {slack_username}")
