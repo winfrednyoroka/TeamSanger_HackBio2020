70dcf75c6d1683f25b25e179e274b061cab42c90
Raplic <69117766+Raplic@users.noreply.github.com>
2020-08-06 16:37:07 +0100

    script_Semilogo.py
    Python script for HackBio Project

---
+++ script_Semilogo.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python3
+
+name = "Semilogo Sonaike"
+email = "semilogosonaike@gmail.com"
+language = "Python"
+biostack = "Genomics"
+slack = "@Semilogo"
+print("{}, {}, {}, {}, {}".format(name, email, language, biostack, slack))
