ea3938a8091a57fe676ad071d06cf7aec5ffdc6f
chavhanrj <69244764+chavhanrj@users.noreply.github.com>
2020-08-06 10:24:15 +0530

    Update script_Rajesh.py

--- script_Rajesh.py
+++ script_Rajesh.py
@@ -4,5 +4,5 @@ name="Rajesh_chavhan"
 email="rajshrichavhan143@gmail.com"
 language="python3"
 biostack="genomics"
-slack_username="Rajesh"
+slack_username="@Rajesh"
 print(f"Name:{name}\n Email:{email}\n Language:{language}\n Biostack:{biostack}\n Slack_username:{slack_username}")
