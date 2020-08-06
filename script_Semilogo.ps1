9fa44d34f0e64b02b1bfe8103a3332796b491a6d
Maruf Ahmed Bhuiyan <dr.marufahmed89@gmail.com>
2020-08-06 18:47:56 +0600

    Update script_Semilogo.ps1

--- script_Semilogo.ps1
+++ script_Semilogo.ps1
@@ -1,9 +1,17 @@
+#!/usr/local/bin/pwsh-preview
+
 $name = "Semilogo Sonaike"
+
 $email = "semilogosonaike@gmail.com"
+
 $language = "PowerShell"
+
 $biostack = "Genomics"
+
 $slackUsername = "@Semilogo"
 
+
 $output = $name + "," + $email + "," + $language + "," + $biostack + "," + $slackUsername
+
 
 $output
