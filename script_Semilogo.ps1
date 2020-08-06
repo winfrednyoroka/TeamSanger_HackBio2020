fd9b2204a5e2494e74e490c839d8589c87785866
Raplic <69117766+Raplic@users.noreply.github.com>
2020-08-06 11:31:05 +0100

    script_Semilogo.ps1
    PowerShell script for Hack-Bio #teamsanger. 
    
    Please you must have powershell installed for Mac or Linux Users.
    Mac: https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-macos?view=powershell-6
    Linux: https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-linux?view=powershell-6
    
    For Windows users, run the shell as administrator and use command: Set-ExecutionPolicy RemoteSigned
    Set Exection Policy to Yes. This is necessary to run powershell scripts on Windows OS

---
+++ script_Semilogo.ps1
@@ -0,0 +1,9 @@
+$name = "Semilogo Sonaike"
+$email = "semilogosonaike@gmail.com"
+$language = "PowerShell"
+$biostack = "Genomics"
+$slackUsername = "@Semilogo"
+
+$output = $name + "," + $email + "," + $language + "," + $biostack + "," + $slackUsername
+
+$output
